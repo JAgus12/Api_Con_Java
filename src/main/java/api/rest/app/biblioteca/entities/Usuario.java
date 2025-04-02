af75c3b440202f1e7a1f65df32cb1e5079551d11
JAgus12 <gonzalezjagustin42@gmail.com>
2025-04-02 15:01:32 +0200

    conexion probada

---
+++ src/main/java/api/rest/app/biblioteca/entities/Usuario.java
@@ -0,0 +1,114 @@
+package api.rest.app.biblioteca.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    private String usuario;
+    private String contraseña;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String email;
+    private Integer telefono;
+    private LocalDate fechaNacimiento;
+    private LocalDate fechaAlta;
+    private String Rol;
+    private boolean activo;
+
+    
+    public Usuario() {
+    }
+
+    public Usuario(String usuario, String contraseña, String nombre, String apellido1, String apellido2, String email,
+            Integer telefono, LocalDate fechaNacimiento, LocalDate fechaAlta, String rol, boolean activo) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaAlta = fechaAlta;
+        Rol = rol;
+        this.activo = activo;
+    }
+
+
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido1() {
+        return apellido1;
+    }
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+    public String getApellido2() {
+        return apellido2;
+    }
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Integer getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+    public void setFechaAlta(LocalDate fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+    public String getRol() {
+        return Rol;
+    }
+    public void setRol(String rol) {
+        Rol = rol;
+    }
+    public boolean isActivo() {
+        return activo;
+    }
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    
+}
