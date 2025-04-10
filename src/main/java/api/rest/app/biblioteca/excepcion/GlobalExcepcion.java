fa0cc0cc9666d66b3b55bd60848f9573668b9e7a
JAgus12 <gonzalezjagustin42@gmail.com>
2025-04-10 16:39:12 +0200

    probar excepcion

--- src/main/java/api/rest/app/biblioteca/excepcion/GlobalExcepcion.java
+++ src/main/java/api/rest/app/biblioteca/excepcion/GlobalExcepcion.java
@@ -1,8 +1,10 @@
 package api.rest.app.biblioteca.excepcion;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +16,9 @@ public class GlobalExcepcion {
     public ResponseEntity<Map<String,Object>> datoNoEncontrado(NotFoundExcepcion ex){
         Map<String,Object> respuesta=new HashMap<>();
         respuesta.put("mensaje", ex.getMensaje());
+        respuesta.put("timestamp", new Timestamp(System.currentTimeMillis()));
+        respuesta.put("codigo", HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(respuesta);
     }
 }
